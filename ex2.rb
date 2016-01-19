bb024fb39f753967caaff850dff9f03e1adafad1
unknown <prabha@juhomi.com>
2016-01-19 08:17:22 +0530

    session_1

---
+++ ex2.rb
@@ -0,0 +1,3 @@
+puts "This will work" # and the comment after is ignored
+# puts "This won't run."
+puts "This will run."
---
+++ ex5.rb
@@ -0,0 +1,18 @@
+my_name = 'Praba.K'
+my_age = 24 
+my_height = 64
+my_weight = 80 
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "Let's talk about #{my_name}"
+puts "She's #{my_height} inches tall."
+puts "She's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "She's got #{my_eyes} eyes and #{my_hair} hair."
+puts "Her teeth are usually #{my_teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
+puts "If I add Age : #{my_age}, Height : #{my_height}, and Weight : #{my_weight} I get #{my_age + my_height + my_weight}."
